3c6d6e3672e90bf199539e2975bf437253317400
rehmanshawon <meghnadbadh@gmail.com>
2023-07-30 15:34:12 +0600

    first commit

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  serverRuntimeConfig: {
+    dbConfig: {
+      host: "3.92.234.242",
+      port: 3306,
+      user: "borna",
+      password: "ApsisINT@123", // @@@
+      database: "kopotron_example_next",
+    },
+    secret:
+      "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING",
+  },
+  publicRuntimeConfig: {
+    apiUrl:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/api" // development api
+        : "http://localhost:3000/api", // production api
+  },
+};
+
+module.exports = nextConfig;
