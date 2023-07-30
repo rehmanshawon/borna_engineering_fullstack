79bfeabb634d7d2751aa311953339f35a739fa05
rehmanshawon <meghnadbadh@gmail.com>
2023-07-31 05:21:04 +0600

    aaa

--- next.config.js
+++ next.config.js
@@ -8,7 +8,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "3.95.176.100:3000" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "http://3.95.176.100:3000",
+          }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
