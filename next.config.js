3237c5b5b7e8b7b4b1ab4333eee20e280bc67b87
rehmanshawon <meghnadbadh@gmail.com>
2023-07-30 21:50:23 +0600

    cc

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
       // host: "3.92.234.242",
       host: "localhost",
       port: 3306,
-      user: "borna",
+      user: "root",
       password: "ApsisINT@123", // @@@
       database: "kopotron_example_next",
     },
@@ -16,8 +16,8 @@ const nextConfig = {
   publicRuntimeConfig: {
     apiUrl:
       process.env.NODE_ENV === "development"
-        ? "http://44.201.134.16:3000/api" // development api
-        : "http://44.201.134.16:3000/api", // production api
+        ? "http://localhost:3000/api" // development api
+        : "http://localhost:3000/api", // production api
   },
 };
 
