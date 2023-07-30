e28e5d882430a03330f07296356924b2ec9d6a75
rehmanshawon <meghnadbadh@gmail.com>
2023-07-31 01:30:28 +0600

    bad gateway

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
       // host: "3.92.234.242",
       host: "127.0.0.1",
       port: 3306,
-      user: "root",
+      user: "borna",
       password: "ApsisINT@123", // @@@
       database: "kopotron_example_next",
     },
@@ -16,8 +16,8 @@ const nextConfig = {
   publicRuntimeConfig: {
     apiUrl:
       process.env.NODE_ENV === "development"
-        ? "http://localhost:3000/api" // development api
-        : "http://localhost:3000/api", // production api
+        ? "http://3.95.176.100/:3000/api" // development api
+        : "http://3.95.176.100/:3000/api", // production api
   },
 };
 
