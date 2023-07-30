4ec9984d4809292b2b9c5f2d5b5c145b6b0053b5
rehmanshawon <meghnadbadh@gmail.com>
2023-07-31 04:59:31 +0600

    aaa

--- next.config.js
+++ next.config.js
@@ -4,10 +4,11 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/(.*)",
+        // matching all API routes
+        source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,DELETE,PATCH,POST,PUT",
