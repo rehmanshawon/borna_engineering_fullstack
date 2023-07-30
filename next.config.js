0bae5bfc64b9a96a61f41da20ba76ec64b49af50
rehmanshawon <meghnadbadh@gmail.com>
2023-07-31 05:24:22 +0600

    sdfs

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "http://3.95.176.100:3000",
+            value: "https://3.95.176.100",
           }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
