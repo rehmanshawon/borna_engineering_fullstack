b4dbef455957e670f3240725f52a5af900e652ac
rehmanshawon <meghnadbadh@gmail.com>
2023-07-31 04:56:11 +0600

    aaa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
   serverRuntimeConfig: {
     dbConfig: {
       // host: "3.92.234.242",
@@ -19,24 +39,6 @@ const nextConfig = {
         ? "http://3.95.176.100:3000/api" // development api
         : "http://3.95.176.100:3000/api", // production api
   },
-  headers: [
-    {
-      source: "/api/(.*)",
-      headers: [
-        { key: "Access-Control-Allow-Credentials", value: "true" },
-        { key: "Access-Control-Allow-Origin", value: "*" },
-        {
-          key: "Access-Control-Allow-Methods",
-          value: "GET,DELETE,PATCH,POST,PUT",
-        },
-        {
-          key: "Access-Control-Allow-Headers",
-          value:
-            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-        },
-      ],
-    },
-  ],
 };
 
 module.exports = nextConfig;
